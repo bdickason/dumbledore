4e0b107f74d75e465b2012e0cd57d2367a775409
Brad Dickason <brad.dickason@oculus.com>
2017-11-17 01:08:25 -0800

    Can now start the bot with .env files

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+require('dotenv').config();	// Load environment variables from .env
+
 var hpcbot = require('../hpc-bot');
 
-hpcbot();
+// Initialize bot config
+var options = {
+  username: process.env.HPC_USERNAME,
+  oauth: process.env.HPC_PASSWORD,
+  channel: process.env.HPC_CHANNEL,
+  clientID: process.env.HPC_CLIENTID,
+  mixpanel: process.env.HPC_MIXPANEL
+};
+
+hpcbot(options);
