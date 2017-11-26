9349e61a7ce502d1dd1c9421a620e3b367d7fda0
Brad Dickason <brad.dickason@oculus.com>
2017-11-26 13:20:15 -0800

    Split out hpc-specific commands into dumbledore repo

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+var fs = require('fs');
 require('dotenv').config();	// Load environment variables from .env
 
-// var hpcbot = require('../hpc-bot');  // For testing
-var hpcbot = require('hpc-bot');
+var hpcbot = require('../hpc-bot');  // For testing
+// var hpcbot = require('hpc-bot');
 
 // Shared eventbus for passing around events
 var EventEmitter = require('events');
@@ -14,14 +15,85 @@ var options = {
   channel: process.env.HPC_CHANNEL,
   clientID: process.env.HPC_CLIENTID,
   mixpanel: process.env.MIXPANEL,
+  whitelist: require('./config/whitelist.json'),
   eventbus: events
 };
 
 // Initialize bot
 var bot = new hpcbot(options);
 
-// Setup chat listeners
-
-// Setup overlays
+/* Setup overlays */
 var overlays = require('./overlays');
 bot.overlays.add(overlays);
+
+/* Setup commands */
+// !rules - Shows the rules of the HPC
+var rules = require('./lib/rules');
+rules.start(options.eventbus);
+bot.commands.add(rules.commands);
+
+// Message on Join
+var Join = require('./lib/join');
+Join.start(options.eventbus, bot.User);
+
+// Goblin Gold
+var Gold = require('./lib/gold');
+Gold.start(options.eventbus, bot.Resource, bot.Channel);
+
+// !setcommends / !commends
+var Commends = require('./lib/commends');
+Commends.start(options.eventbus, bot.User);
+// commands.push(Commends);
+
+// !house / !sortinghat
+var House = require('./lib/house');
+House.start(options.eventbus, bot.User);
+bot.overlays.add(House.overlay);
+// commands.push(House);
+
+// !status
+var Status = require('./lib/status');
+Status.start(options.eventbus, bot.User);
+// commands.push(Status);
+
+// !dice / !coin
+var Random = require('./lib/random');
+Random.start(options.eventbus);
+// commands.push(Random);
+
+// !cup
+var Housecup = require('./lib/housecup');
+Housecup.start(options.eventbus, bot.Team);
+// commands.push(Housecup);
+
+// !hween
+var Halloween = require('./lib/halloween');
+Halloween.start(options.eventbus, bot.Team);
+// commands.push(Halloween);
+
+// Subscribers
+var Subscribe = require('./lib/subscribe');
+Subscribe.start(options.eventbus);
+
+// Quidditch
+var Quidditch = require('./lib/quidditch');
+Quidditch.start(options.eventbus, bot.Team);
+bot.overlays.add(Quidditch.overlay);
+
+// !text (External module)
+var Text = require('./lib/text');
+Text.start(options.eventbus);
+
+
+// Quickly load all commands?
+// fs.readdir('./lib', function(err, directories) {
+//   if(err) {
+//     console.error('There was an issue reading the commands directory', err);
+//   }
+//   directories.forEach(function(directory) {
+//     // 1. Require the module
+//     var tmp = require('./lib/' + directory);
+//     // Register chat listeners
+//     bot.commands.add(tmp.commands);
+//   });
+// });
